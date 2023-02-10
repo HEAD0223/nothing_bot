2ba08508d156b48f4aadcdcc225c333b24eead94
HEAD0223 <andrei.dolgopol2000@gmail.com>
2023-02-10 14:41:15 +0200

    Update polling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 async def on_startup(dp):
-    await bot.set_webhook(config.WEBHOOK_URL)
+    # await bot.set_webhook(config.WEBHOOK_URL)
 
     from utils.notify_admins import on_startup_notify
     await on_startup_notify(dp)
@@ -8,7 +8,7 @@ async def on_startup(dp):
 
 async def on_shutdown(dp):
     logging.warning('Shutting down..')
-    await bot.delete_webhook()
+    # await bot.delete_webhook()
     logging.warning('Bye!')
 
 
@@ -21,11 +21,12 @@ if __name__ == '__main__':
     from handlers.middlewares import ThrottlingMiddleware
 
     dp.middleware.setup(ThrottlingMiddleware())
-    executor.start_webhook(
-        dispatcher = dp,
-        webhook_path=config.WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=config.WEBAPP_HOST,
-        port=config.WEBAPP_PORT)
+    # executor.start_webhook(
+    #     dispatcher = dp,
+    #     webhook_path=config.WEBHOOK_PATH,
+    #     on_startup=on_startup,
+    #     on_shutdown=on_shutdown,
+    #     skip_updates=True,
+    #     host=config.WEBAPP_HOST,
+    #     port=config.WEBAPP_PORT)
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown, skip_updates=True)
